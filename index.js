d6d22c06c7e5dcfb217b3d481c7baeb18bb44e6d
tishalewis8707 <tishalewis8707@gmail.com>
2021-04-04 23:12:26 -0400

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function  destructivelyAppendDriver (name) {
+drivers.push(name)
+}
+// removes the last driver from the drivers array
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+//adds to beginning of the drivers array
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name)
+}
+//
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name)
+}
+
+function appendDriver(name) {
+    const driversArray = [...drivers,name];
+    return driversArray;
+}
+//returns a new array, leaving the drivers array unchanged
+
+function prependDriver(name) {
+    const driversArray = [name,...drivers];
+    return driversArray; 
+}
+//array unchanged 
+
+function removeLastDriver(name) {
+    const driversArray = [...drivers];
+    driversArray.pop();
+    return driversArray;
+}
+
+function removeFirstDriver(name) {
+    const driversArray = [...drivers];
+    driversArray.shift();
+    return driversArray;
+}
